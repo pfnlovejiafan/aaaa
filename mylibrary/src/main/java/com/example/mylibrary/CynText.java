641dab2f31832503f84312398d168faf6847ae3b
mettle001 <540435039@qq.com>
2023-06-06 15:32:38 +0800

    ceshi

---
+++ mylibrary/src/main/java/com/example/mylibrary/CynText.java
@@ -0,0 +1,16 @@
+package com.example.mylibrary;
+
+/**
+ * 项目名称：GitAndroidK
+ * 创建人： PANZERS
+ * 创建时间：2023/6/611:48
+ * 修改人：PANZERS
+ * 修改时间：2023/6/611:48
+ * 修改备注：
+ */
+public class CynText {
+    public int calcuation(int number){
+        return number*2;
+
+    }
+}
